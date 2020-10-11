3e2e4d1215cd002ccb326ca9111b4900d87455e6
Loic Laureote <laureote-loic@hotmail.com>
2020-10-11 22:38:49 +0200

    add another script

--- scripts/watermark/watermark_dataset.py
+++ scripts/watermark/watermark_dataset.py
@@ -7,7 +7,7 @@ import numpy as np
 import argparse
 import cv2
 import os
-
+import imutils
 font = cv2.FONT_HERSHEY_SIMPLEX
 import numpy as np
 import cv2
@@ -15,6 +15,15 @@ import cv2
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 600)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 400)
+
+import numpy as np
+
+
+
+
+greenLower = np.array([255, 50, 50]) # B, G, R
+greenUpper = np.array([51, 0, 0])
+
 
 watermark = cv2.imread("./pyimagesearch_watermark.png", cv2.IMREAD_UNCHANGED)
 (wH, wW) = watermark.shape[:2]
@@ -32,6 +41,28 @@ while(True):
 	ret, image = cap.read()
 	(h, w) = image.shape[:2]
 	image = np.dstack([image, np.ones((h, w), dtype="uint8") * 255])
+
+
+	imag = imutils.resize(image, width=600)
+	imag = cv2.GaussianBlur(imag, (11, 11), 0)
+	hsv = cv2.cvtColor(imag, cv2.COLOR_BGR2HSV)
+	mask = cv2.inRange(hsv, greenLower, greenUpper)
+	mask = cv2.erode(mask, None, iterations=2)
+	mask = cv2.dilate(mask, None, iterations=2)
+
+
+	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+	center = None
+	if len(cnts) > 0:
+		print(cnts)
+		c = max(cnts, key=cv2.contourArea)
+		((x, y), radius) = cv2.minEnclosingCircle(c)
+		if radius > 1:
+			#cv2.circle(image, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+			cv2.circle(image, (int(x), int(y)), int(radius),
+				(0, 255, 255), 2)
+			cv2.circle(image, center, 5, (0, 0, 255), -1)
+
 
 	overlay = np.zeros((h, w, 4), dtype="uint8")
 	overlay[h - wH - 10:h - 10, w - wW - 10:w - 10] = watermark
